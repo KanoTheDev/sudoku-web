2b9972cf6445acc63057ca7f3de129a31b558fd6
KarolisNo <kano@kano.lt>
2014-03-20 18:07:30 +0000

    Hiding zeros in unsolved solution

--- sudoku.rb
+++ sudoku.rb
@@ -2,23 +2,28 @@ require 'sinatra'
 require_relative './lib/sudoku'
 require_relative './lib/cell'
 
-get '/' do 
-	@current_solution = puzzle(random_sudoku)
+enable :sessions
+
+
+get '/' do
+  sudoku = random_sudoku
+  session[:solution] = sudoku
+  @current_solution = puzzle(sudoku)
   erb :index
 end
 
-def unsolved_sudoku
-	@unsolved_sudoku = random_sudoku
-
-end
-
 def random_sudoku
-	seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
-	sudoku = Sudoku.new(seed.join)
-	sudoku.solve!
-	sudoku.to_s.chars
+  seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
+  sudoku = Sudoku.new(seed.join)
+  sudoku.solve!
+  sudoku.to_s.chars
 end
 
 def puzzle(sudoku)
   sudoku.map! {|x| rand < 0.3 ? 0 : x} 
 end
+
+
+def cell_value(value)
+   value.to_i == 0 ? '' : value
+end  
