d540b47aaef2575f4a9fcd6e57594afad66b3053
Karolis Noreika <kano@kano.lt>
2014-05-23 18:10:05 +0300

    difficulty buttons

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
-gem 'sinatra' 
+ruby "2.0.0"
+gem 'sinatra'
 gem 'shotgun'
 gem 'sinatra-partial'
 gem 'rack-flash3'
+gem 'unicorn'
 gem 'newrelic_rpm'
--- sudoku.rb
+++ sudoku.rb
@@ -1,16 +1,67 @@
 require 'sinatra'
-require_relative './lib/sudoku'
-require_relative './lib/cell'
 require 'sinatra/partial'
 require 'rack-flash'
-require 'newrelic_rpm'
+require 'bundler/setup'
+require "newrelic_rpm"
+require_relative './lib/sudoku'
+require_relative './lib/cell'
+require_relative './helpers/application.rb'
 
-use Rack::Flash 
-
-set :session_secret, "secretkey"
-
-set :partial_template_engine, :erb
 enable :sessions
+set :partial_template_engine, :erb
+use Rack::Flash
+
+set :session_secret, 'hardpass'
+
+def random_sudoku
+  # seed = (1..9).to_a + Array.new(81-9, 0) shuffling entire ray pushes more processing onto server, however doesn't add any benefit
+  # sudoku = Sudoku.new(seed.shuffle.join)
+  seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
+  sudoku = Sudoku.new(seed.join)
+  sudoku.solve!
+  sudoku.to_s.chars
+end
+
+# this method removes some digits from the solution to create a puzzle
+def puzzle(sudoku,difficulty)
+  sudoku.each_slice(3).to_a.each{|e|
+    e[[0,1,2].sample] = "0"
+    e[[0,1,2].sample] = "0" if difficulty == :medium || difficulty == :hard
+    e[[0,1,2].sample] = "0" if difficulty == :hard
+  }.flatten
+end
+
+def box_order_to_row_order(cells)
+  boxes = cells.each_slice(9).to_a
+  (0..8).to_a.inject([]) {|memo, i|
+    first_box_index = i / 3 * 3
+    three_boxes = boxes[first_box_index, 3]
+    three_rows_of_three = three_boxes.map do |box|
+      row_number_in_a_box = i % 3
+      first_cell_in_the_row_index = row_number_in_a_box * 3
+      box[first_cell_in_the_row_index,3]
+    end
+    memo += three_rows_of_three.flatten
+  }
+end
+
+def generate_new_puzzle_if_necessary
+  return if session[:current_solution]
+  session[:difficulty] = session[:new_difficulty] || :medium
+  sudoku = random_sudoku
+  session[:solution] = sudoku
+  session[:puzzle] = puzzle(sudoku, session[:difficulty])
+  session[:current_solution] = session[:puzzle]
+end
+
+def prepare_to_check_solution
+  @check_solution = session[:check_solution]
+  if @check_solution
+    flash[:incorrect_notice] = "Incorrect values are highlighted in yellow"
+    flash[:correct_notice] = "Correct values are highlighted in green"
+  end
+  session[:check_solution] = nil
+end
 
 get '/' do
   prepare_to_check_solution
@@ -21,76 +72,28 @@ get '/' do
   erb :index
 end
 
-get '/solution' do
-  @current_solution = session[:solution]
-  erb :index
-end
-
-post "/" do
-
-  cells = box_order_to_row_order(params["cell"])  
-  session[:current_solution] = cells.map{|value| value.to_i }.join
-  session[:check_solution] = true
+post '/' do
+  cells = box_order_to_row_order(params["cell"])
+  session[:current_solution] = cells.map{|value| value.to_i}.join if !params[:reset]
+  session[:check_solution] = true if !params[:save] && !params[:reset]
+  session[:current_solution] = session[:puzzle] if params[:reset]
   redirect to("/")
 end
 
-def generate_new_puzzle_if_necessary
-  return if session[:current_solution]
-  sudoku = random_sudoku
-  session[:solution] = sudoku
-  session[:puzzle] = puzzle(sudoku)
-  session[:current_solution] = session[:puzzle]    
+get '/solution' do
+  redirect to("/") if !session[:current_solution]
+  @current_solution = session[:solution]
+  @solution = session[:solution]
+  @puzzle = session[:puzzle]
+  erb :index
 end
 
-def random_sudoku
-  seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
-  sudoku = Sudoku.new(seed.join)
-  sudoku.solve!
-  sudoku.to_s.chars
+get '/help' do
+  erb :help
 end
 
-def puzzle(sudoku)
-  sudoku.map {|x| rand < 0.3 ? 0 : x} 
-end
-
-def box_order_to_row_order(cells)
-  boxes = cells.each_slice(9).to_a
-  (0..8).to_a.inject([]) {|memo, i|
-    first_box_index = i / 3 * 3
-    three_boxes = boxes[first_box_index, 3]  
-    three_rows_of_three = three_boxes.map do |box| 
-      row_number_in_a_box = i % 3
-      first_cell_in_the_row_index = row_number_in_a_box * 3
-      box[first_cell_in_the_row_index, 3]
-    end
-    memo += three_rows_of_three.flatten
-  }
-end 
-
-def prepare_to_check_solution
-  @check_solution = session[:check_solution]
-  if @check_solution
-    flash[:notice] = "Incorrect values are highlighted in yellow"
-  end
-  session[:check_solution] = nil
-end
-
-helpers do
-
-  def colour_class(solution_to_check, puzzle_value, current_solution_value, solution_value)
-    must_be_guessed = puzzle_value == 0
-    tried_to_guess = current_solution_value.to_i != 0
-    guessed_incorrectly = current_solution_value != solution_value
-
-    if solution_to_check && must_be_guessed && tried_to_guess && guessed_incorrectly
-      'incorrect'
-    elsif !must_be_guessed
-      'value-provided'
-    end
-  end
-
-  def cell_value(value)
-    value.to_i == 0 ? '' : value
-  end
-
+post '/set' do
+  session[:new_difficulty] = params[:difficulty].to_sym
+  session[:current_solution] = nil
+  redirect to("/")
 end
